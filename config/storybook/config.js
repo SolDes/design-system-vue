5078328c8b1e1b8e18779822e915fc4a2bcbcd56
Daniel Ferro <dlcerva@gmail.com>
2019-02-04 11:55:13 -0500

    Added sample tests.

--- config/storybook/config.js
+++ config/storybook/config.js
@@ -2,7 +2,8 @@
 import { configure } from '@storybook/vue';
 import '@salesforce-ux/design-system/assets/styles/salesforce-lightning-design-system.min.css';
 
-const req = require.context('../../src/stories', true, /.stories.js$/);
+
+const req = require.context('../../src/components/', true, /stories\.js$/);
 
 function loadStories() {
   req.keys().forEach(filename => req(filename));
